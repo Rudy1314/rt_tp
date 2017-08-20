9d3f3b2f489987724edcee222cf6600964cd1879
RTX <2393482408@qq.com>
2017-08-20 22:15:30 +0800

    add other module

--- index.php
+++ index.php
@@ -16,11 +16,18 @@ if (version_compare(PHP_VERSION, '5.3.0', '<')) {
     die('require PHP > 5.3.0 !');
 }
 
+// 绑定Admin模块到当前入口文件
+//define('BIND_MODULE','Admin');
+//define('BUILD_CONTROLLER_LIST','Index,User,Menu');
+
 // 开启调试模式 建议开发阶段开启 部署阶段注释或者设为false
 define('APP_DEBUG', true);
 
 // 定义应用目录
 define('APP_PATH', './Application/');
+
+//定义运行时的目录
+//define('RUNTIME_PATH','./Runtime/');
 
 // 引入ThinkPHP入口文件
 require './ThinkPHP/ThinkPHP.php';
